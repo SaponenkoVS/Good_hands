85dd3a67b25f786d54dba01ba181cede5b5d9702
vlad <saponenkovladislav2001@mail.ru>
2023-05-27 13:39:49 +0200

    Initial commit

---
+++ backend/src/app.service.ts
@@ -0,0 +1,12 @@
+import {Injectable} from '@nestjs/common';
+import {PrismaService} from "./prisma/prisma.service";
+
+@Injectable()
+export class AppService {
+    constructor(private prismaService: PrismaService) {
+    }
+
+    getHello() {
+        return this.prismaService.user.findMany()
+    }
+}
